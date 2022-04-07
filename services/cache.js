fd985a2508edd0b99b4eacd47bc191f0feb45806
Leonard <leonardwkee@gmail.com>
2022-04-06 20:26:18 -0700

    add comments to help explain

--- services/cache.js
+++ services/cache.js
@@ -5,8 +5,13 @@ const util = require('util')
 const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl)
 client.get = util.promisify(client.get)
+
+// save original
 const exec = mongoose.Query.prototype.exec
 
+// need to overwrite mongoose's exec to
+// - add custom reusable caching solution
+// - pass new & custom parts to mongoose's original exec
 mongoose.Query.prototype.exec = async function () {
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
